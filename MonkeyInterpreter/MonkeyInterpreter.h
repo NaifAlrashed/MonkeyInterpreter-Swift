8d778e59bb9bccd83d11b0a847835dd7315680d7
NaifAlrashed <naialrashed@alfaisal.edu>
2019-03-01 15:25:26 +0300

    Initial Commit

---
+++ MonkeyInterpreter/MonkeyInterpreter.h
@@ -0,0 +1,19 @@
+//
+//  MonkeyInterpreter.h
+//  MonkeyInterpreter
+//
+//  Created by Naif Alrashed on 01/03/2019.
+//  Copyright © 2019 Naif Alrashed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MonkeyInterpreter.
+FOUNDATION_EXPORT double MonkeyInterpreterVersionNumber;
+
+//! Project version string for MonkeyInterpreter.
+FOUNDATION_EXPORT const unsigned char MonkeyInterpreterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MonkeyInterpreter/PublicHeader.h>
+
+
