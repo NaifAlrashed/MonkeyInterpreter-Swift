2988d3eb100411f34071844582e556f1a851ff9c
NaifAlrashed <naialrashed@alfaisal.edu>
2019-03-29 18:40:06 +0300

    remove unused code

--- MonkeyInterpreter/Parser/Parser.swift
+++ MonkeyInterpreter/Parser/Parser.swift
@@ -6,23 +6,12 @@
 //  Copyright © 2019 Naif Alrashed. All rights reserved.
 //
 
-import Foundation
-
 struct Parser {
     
     private var lexer: Lexer
     
     lazy var tokens: [Token] = {
         return lexer.tokenize()
-    }()
-    
-    lazy var currentToken: Token = {
-        guard let token = tokens.first else { return .EOF }
-        return token
-    }()
-    
-    lazy var peekToken: Token =  {
-        return tokens.count >= 2 ? tokens[1]: .EOF
     }()
     
     init(lexer: Lexer) {
--- MonkeyInterpreter/Parser/Statement.swift
+++ MonkeyInterpreter/Parser/Statement.swift
@@ -2,7 +2,7 @@
 //  Statement.swift
 //  MonkeyInterpreter
 //
-//  Created by Naif Alrashed on 12/03/2019.
+//  Created by Naif Alrashed on 29/03/2019.
 //  Copyright © 2019 Naif Alrashed. All rights reserved.
 //
 
